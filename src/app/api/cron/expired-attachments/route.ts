d100d6afe5796094a9010883580112a18d18111b
Paul George <paultibulca@gmail.com>
2024-08-26 23:38:04 +0300

    Vercel cron once a day

--- src/app/api/cron/expired-attachments/route.ts
+++ src/app/api/cron/expired-attachments/route.ts
@@ -1,10 +1,41 @@
+export const dynamic = "force-dynamic";
+
 import { db } from "@/server/db";
 import { env } from "@/env";
 import { DateTime } from "luxon";
+import { DeleteObjectCommand, S3 } from "@aws-sdk/client-s3";
 
 export async function GET() {
   try {
     console.log(`[${DateTime.now().toISO()}] - Starting cron job`);
+    const attachments = await db.attachment.findMany({
+      where: {
+        confirmed: false,
+        createdAt: {
+          lt: DateTime.now().minus({ minutes: env.R2_FILE_TTL }).toJSDate(),
+        },
+      },
+    });
+
+    const s3 = new S3({
+      endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+      region: env.R2_REGION,
+      credentials: {
+        accessKeyId: env.R2_ACCESS_KEY_ID,
+        secretAccessKey: env.R2_SECRET_ACCESS_KEY,
+      },
+    });
+
+    await Promise.all(
+      attachments.map(async (attachment) => {
+        const command = new DeleteObjectCommand({
+          Bucket: env.R2_BUCKET_NAME,
+          Key: attachment.key,
+        });
+        await s3.send(command);
+      }),
+    );
+
     const deletedAttachments = await db.attachment.deleteMany({
       where: {
         confirmed: false,
