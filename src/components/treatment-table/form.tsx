ca49e7de6f16cb0f7ce7fb81ff1c6852fdcebd39
Paul George <paultibulca@gmail.com>
2024-09-18 19:34:11 +0300

    Added components in form

--- src/components/treatment-table/form.tsx
+++ src/components/treatment-table/form.tsx
@@ -9,10 +9,14 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { api } from "@/trpc/react";
+import type { Material } from "@prisma/client";
+import { Pill, Trash } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { ServiceUnitSchema } from "prisma/generated/zod";
+import { useStateful } from "react-hanger";
 import type { UseFormReturn } from "react-hook-form";
 import { z } from "zod";
+import { AutoComplete } from "../ui/autocomplete";
 import { Button } from "../ui/button";
 import NumberInput from "../ui/number-input";
 import OptionalInputTag from "../ui/optional-input-tag";
@@ -24,6 +28,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
+import { Separator } from "../ui/separator";
 import { TagInput } from "../ui/tag-input";
 import { Textarea } from "../ui/textarea";
 
@@ -164,6 +169,9 @@ export default function ServiceForm({
               </FormItem>
             )}
           />
+          {!form.watch("relatedServices")?.length && (
+            <ComponentsSection form={form} />
+          )}
         </span>
         <FormField
           name="tags"
@@ -185,6 +193,144 @@ export default function ServiceForm({
         />
       </form>
     </Form>
+  );
+}
+
+function ComponentsSection({ form }: { form: UseFormReturn<FormValues> }) {
+  const t = useTranslations("page.treatments.fields");
+  const { data: materials, isLoading } = api.material.list.useQuery({});
+
+  const materialsLenth = form.watch("materials")?.length ?? 0;
+
+  const removeMaterial = (index: number) => {
+    const materials = form.watch("materials");
+    if (!materials) return;
+    const newMaterials = materials.filter((_, i) => i !== index);
+    form.setValue("materials", newMaterials, { shouldDirty: true });
+  };
+
+  return (
+    <>
+      <Separator className="col-span-2" />
+      <span className="col-span-2">
+        <span className="w-full p-4 grid grid-cols-[auto_1fr] gap-4">
+          <Pill className="size-10 p-2 rounded-lg bg-background/80 text-muted-foreground border border-border" />
+          <span className="vertical truncate">
+            <p className="font-medium">Components used</p>
+            <p className="text-muted-foreground text-xs">
+              Add every component used in the service.
+            </p>
+          </span>
+        </span>
+      </span>
+      {form.watch("materials")?.map((_, index) => (
+        <>
+          <span
+            key={`${index}.name`}
+            className="w-full border border-input rounded-md horizontal center-v gap-2 shadow-sm border-dashed"
+          >
+            <Button
+              type="button"
+              variant="ghost"
+              size="icon"
+              className="hover:text-destructive hover:bg-transparent"
+              onClick={() => {
+                removeMaterial(index);
+              }}
+            >
+              <Trash className="size-4" />
+            </Button>
+            <p>
+              {
+                materials?.content.find(
+                  (material) =>
+                    material.id === form.watch(`materials.${index}.materialId`),
+                )?.name
+              }
+            </p>
+          </span>
+          <FormField
+            key={`${index}.quantity`}
+            name={`materials.${index}.quantity`}
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <NumberInput
+                  id={`materials.${index}.quantity`}
+                  min={1}
+                  max={20}
+                  defaultValue={1}
+                  {...field}
+                  className="w-20 text-center"
+                />
+              </FormItem>
+            )}
+          />
+        </>
+      ))}
+      <SearchMaterial
+        form={form}
+        index={materialsLenth}
+        isLoading={isLoading}
+        options={
+          materials?.content.filter(
+            (material) =>
+              !form
+                .watch("materials")
+                .some((m) => m.materialId === material.id),
+          ) ?? []
+        }
+      />
+    </>
+  );
+}
+
+type SearchMaterialProps = {
+  form: UseFormReturn<FormValues>;
+  index: number;
+  className?: string;
+  isLoading: boolean;
+  options: Material[];
+};
+
+function SearchMaterial({
+  form,
+  index,
+  isLoading,
+  options,
+}: SearchMaterialProps) {
+  const search = useStateful("");
+
+  return (
+    <AutoComplete<Material>
+      id="name"
+      search={search.value}
+      setSearch={(_: string) => {}}
+      disabled={isLoading}
+      emptyMessage="No materials found"
+      placeholder="Search for a material..."
+      options={
+        options.map((material) => ({
+          label: material.name,
+          value: material,
+        })) ?? []
+      }
+      onValueChange={(option) => {
+        form.setValue(`materials.${index}.materialId`, option.value.id, {
+          shouldDirty: true,
+        });
+        form.setValue(
+          `materials.${index}.unit_price`,
+          option.value.unit_price,
+          {
+            shouldDirty: true,
+          },
+        );
+        form.setValue(`materials.${index}.quantity`, 1, {
+          shouldDirty: true,
+        });
+      }}
+    />
   );
 }
 
@@ -230,7 +376,7 @@ function ServicesSection({
           className="text-sm text-destructive"
           type="button"
           onClick={() => {
-            form.setValue("relatedServices", []);
+            form.setValue("relatedServices", [], { shouldDirty: true });
           }}
         >
           Remove
