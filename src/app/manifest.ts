657c94bf9bef28ea5b871cb51367c14a004a834a
Paul George <paultibulca@gmail.com>
2024-09-20 18:58:37 +0300

    manifest fix

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,4 +1,4 @@
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
@@ -7,19 +7,19 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "MyDent is a dental practice management software",
     start_url: "/",
     display: "standalone",
-    background_color: "#ffffff",
+    background_color: "#000000",
     theme_color: "#000000",
     icons: [
       {
         src: "/icon-192x192.png",
         sizes: "192x192",
-        type: "image/png",
+        type: "image/png"
       },
       {
         src: "/icon-512x512.png",
         sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  };
+        type: "image/png"
+      }
+    ]
+  }
 }
