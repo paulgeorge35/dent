5084b5e8d0c74cba2a40c710c227704d0a6e59b6
Paul George <paultibulca@gmail.com>
2024-09-20 18:36:53 +0300

    manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,9 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
+import { useTheme } from "next-themes";
 
 export default function manifest(): MetadataRoute.Manifest {
+  const theme = useTheme();
+  const theme_color = theme.resolvedTheme === "light" ? "#000000" : "#020817";
   return {
     name: "MyDent",
     short_name: "MyDent",
@@ -9,17 +12,17 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
-    // icons: [
-    //   {
-    //     src: "/icon-192x192.png",
-    //     sizes: "192x192",
-    //     type: "image/png"
-    //   },
-    //   {
-    //     src: "/icon-512x512.png",
-    //     sizes: "512x512",
-    //     type: "image/png"
-    //   }
-    // ]
-  }
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
 }
