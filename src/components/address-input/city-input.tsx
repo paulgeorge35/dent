615751f40c7d7ce18f95e7b2fb8502117fd1f2df
Paul George <paultibulca@gmail.com>
2024-10-07 18:12:30 +0300

    fix(main): text-base in city search on mobile

--- src/components/address-input/city-input.tsx
+++ src/components/address-input/city-input.tsx
@@ -89,6 +89,7 @@ export function CitySelect({
                 <CommandInput
                   placeholder={searchPlaceholder}
                   defaultValue={value}
+                  className="text-base"
                 />
                 <Button
                   variant="ghost"
