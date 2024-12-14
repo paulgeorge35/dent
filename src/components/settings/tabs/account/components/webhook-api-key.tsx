9c83220f991147c03badba1318c88717dadc992f
Paul George <paultibulca@gmail.com>
2024-12-14 10:32:06 +0200

    enhanced webhook api key input

--- src/components/settings/tabs/account/components/webhook-api-key.tsx
+++ src/components/settings/tabs/account/components/webhook-api-key.tsx
@@ -9,7 +9,8 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { showErrorToast } from "@/lib/handle-error";
-import { Copy, Plus, RotateCcw } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Copy, Eye, EyeOff, Plus, RotateCcw } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { useBoolean } from "react-hanger";
 
@@ -21,6 +22,7 @@ export default function WebhookApiKeyForm({ webhookApiKey }: WebhookApiKeyFormPr
     const t = useTranslations("page.settings.tabs.account.webhook-api-key");
     const tDialog = useTranslations("page.settings.tabs.account.webhook-api-key.dialog");
     const confirmationDialog = useBoolean(false)
+    const hidden = useBoolean(true);
     const router = useRouter();
 
     const { mutate, isPending } = api.user.updateWebhookApiKey.useMutation({
@@ -45,12 +47,25 @@ export default function WebhookApiKeyForm({ webhookApiKey }: WebhookApiKeyFormPr
                 <CardTitle>{t("title")}</CardTitle>
                 <CardDescription>{t("description")}</CardDescription>
             </CardHeader>
-            <CardContent className="p-6 horizontal gap-2">
-                <span className="horizontal center-v gap-2 border border-input rounded-md grow">
-                    <Input value={webhookApiKey ?? ""} readOnly className="border-none focus-visible:ring-0" />
-                    {webhookApiKey && <Button onClick={onCopy} variant="ghost" className="!p-0 !size-8 m-1">
-                        <Copy className="size-4" />
-                    </Button>}
+            <CardContent className="p-6 horizontal gap-2 items-stretch">
+                <span className="horizontal center-v gap-2 border border-input rounded-md grow group">
+                    <Input value={webhookApiKey ?? ""} type={hidden.value ? "password" : "text"} readOnly className="border-none focus-visible:ring-0" />
+                    <span className={cn("horizontal center-v gap-1 p-1", { "hidden": !webhookApiKey })}>
+                        <Button
+                            onClick={onCopy}
+                            variant="ghost"
+                            className="!p-0 !size-8 group-hover:opacity-100 opacity-0 transition-opacity duration-300"
+                        >
+                            <Copy className="size-4" />
+                        </Button>
+                        <Button
+                            onClick={() => hidden.toggle()}
+                            variant="ghost"
+                            className="!p-0 !size-8 group-hover:opacity-100 opacity-0 transition-opacity duration-300"
+                        >
+                            {hidden.value ? <Eye className="size-4" /> : <EyeOff className="size-4" />}
+                        </Button>
+                    </span>
                 </span>
                 <Button
                     isLoading={isPending}
@@ -59,7 +74,7 @@ export default function WebhookApiKeyForm({ webhookApiKey }: WebhookApiKeyFormPr
                     variant="expandIcon"
                     iconPlacement="left"
                     type="submit"
-                    className="col-span-2 sm:w-fit"
+                    className="col-span-2 sm:w-fit !h-auto"
                 >
                     {webhookApiKey ? t("regenerate") : t("generate")}
                 </Button>
@@ -71,6 +86,6 @@ export default function WebhookApiKeyForm({ webhookApiKey }: WebhookApiKeyFormPr
                     onConfirm={() => mutate()}
                 />
             </CardContent>
-        </Card>
+        </Card >
     );
 }
